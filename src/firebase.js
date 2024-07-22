b4882b70bcb22ad4261cc4c0e702fd885475efc3
nkduyenpham <90058312+nkduyenpham@users.noreply.github.com>
2024-07-22 13:59:50 +0800

    update git ignore file, load sensitive data to .env file

--- src/firebase.js
+++ src/firebase.js
@@ -7,20 +7,21 @@ import { getFirestore, doc, setDoc } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDSQledXkA7JmUL-v8G5K7WKUZwR2puJ2k",
-    authDomain: "personal-finance-tracker-3a4e1.firebaseapp.com",
-    projectId: "personal-finance-tracker-3a4e1",
-    storageBucket: "personal-finance-tracker-3a4e1.appspot.com",
-    messagingSenderId: "824131387541",
-    appId: "1:824131387541:web:34eee4212c913b59259b9c",
-    measurementId: "G-7W2GNQ7EQ0"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app); 
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
-const auth = getAuth(app); 
+const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+
 export { db, auth, provider, doc, setDoc };
